c046d004d4fa3ab7a3a49e45f8decc4d22a9ca24
Kite <[bergman1114@google.com]>
2021-04-15 15:08:04 -0400

    ready to go

--- template.sh
+++ template.sh
@@ -16,24 +16,69 @@
 # Changes Made:                                                                #
 #               April 1st: Created Template				       #
 #		April 3rd: Created Menu order system			       #
+#		April 6th: Revised the Entire Code			       #
+#		April 8th: Modified the Menu Section			       #
 #		April 12th: Code finished w/font revisions		       #
+#		April 15th: Made it look Fancy w/Chicken		       #
 #                                                                              #
 ################################################################################
 ################################################################################
 ################################################################################
 #                                                                              #
-# Insert Index Here                                                            #
+#                               Code Index                                     #
 #                                                                              #
+#	Welcome Screen							       #
+#		--We Welcome our Guests					       #
+#	Log-In Screen							       #
+#		--Exsiting User						       #
+#		--Create User						       #
+#               --Guest							       #
+#	Food Delivery Screen						       #
+#               --Pick-Up						       #
+#               --Delivery						       #
+#	Place Order Screen						       #
+#               --Menu Selection Brief					       #
+#               --Ordering Section					       #
+#			--Wing Size					       #
+#                       --Chocie of Sauce				       #
+#                       --Chocie of Side				       #
+#                       --Chocie of Dressing				       #
+#                       --Chocie of Beverage				       #
+#	Review Order Screen						       #
+#		--Order Summary						       #
+#	Confirmation Screen						       #
+#		--Checkout						       #
+#			--Cash Selection				       #
+#                       --Credit Card Selection				       #
+#		--Invoice Amount					       #
+#		--Confirm Order and Payment				       #
+#	Order Placed							       #
+#	Secret Screen							       #
+#									       #
 ################################################################################
 ################################################################################
 ################################################################################
 #                                                                              #
 #                                                                              #
-# Alias and list of Arrays:                                                    #
+# Alias and list of Declare Arrays:                                            #
+#									       #
+declare -a wing_size=("Small" "Medium" "Large")
+declare -a wing_size_price=("10" "15" "20")
 
-#declare variables with pricing
+declare -a protein_options=("Regular" "Drum_Only" "Flat_Only" "Breaded_Cauliflower")
+declare -a protein_options_price=("0" "0" "0" "0")
 
-#alias
+declare -a sauce_options=("Buffalo" "BBQ" "ThaiCurry" "SpicyGarlic")
+declare -a sauce_options_price=("0" "0" "0" "0")
+
+declare -a side_options=("PotatoWedges" "Fries" "Extra_Sauce" "Celery" "Extra_Wings" "No_Side")
+declare -a side_options_price=("3" "2" "1" "1" "3" "0")
+
+declare -a dressing_options=("Blu_Cheese" "Ranch" "No_Dressing")
+declare -a dressing_options_price=("0" "0" "0")
+
+declare -a beverage_options=("Soda" "SweetTea" "NormalTea" "H2O")
+declare -a beverage_options_price=("2" "3" "2" "1")
 
 ################################################################################
 ################################################################################
@@ -43,29 +88,33 @@
 clear
 
 echo "-------------------------------------------------------------------------"
-toilet --metal "WING'IN IT"
-toilet --metal "w/ Chef"
-toilet --metal "Shweta"
+	toilet --metal "WING'IN IT"
+	toilet --metal "w/ Chef"
+	toilet --metal "Shweta"
 echo "-------------------------------------------------------------------------"
-sleep 1
+
+sleep 10
 clear
+
 echo "Welcome to Wing'In It!"
-sleep 1
+sleep 10
 echo "                      "
 echo "Home to the Very Best Chicken in all of Northern Virginia."
-sleep 1
+sleep 10
 echo "                           "
 echo "Our Mission is to Deliver the Freshest of Ingredients and Fuse Those With Our Passion For Wings."
-sleep 1
+sleep 10
 echo " "
 echo "At Wing'In It, We are All about BIG Portions, Remarkable Flavors, and Crispy Goodness."
-sleep 1
+sleep 10
 echo "                                           "
 echo "To Begin the Fresh Eating and Quality Service Expirecne; please proceed to the Login page."
-sleep 1
+sleep 10
 echo " "
 echo "And Thank YOU for dining with Us Today!"
+sleep 10
 clear
+
 #								               #
 ################################################################################
 ################################################################################
@@ -74,6 +123,7 @@ clear
 #                            Log-in/User Account Screen                        #
 
 function createUserDetails
+
 {
 read -p "Enter FIrst Name >> " firstName
 echo "                          "
@@ -86,7 +136,9 @@ echo "                            "
 read -p "Enter Email >> " email
 echo "                        "
 }
+
 function createUserName
+
 {
 read -p "Enter username to create >> " createUsername
 echo "                                 "
@@ -95,12 +147,14 @@ read -p "Enter password to create >> " createPassword
 
 
 function welcomeMessage
+
 {
 echo -e "\nWelcome to Wing'in it"
 echo -e "\nWe're here for you ,Order now"
 }
 
 function loginMethod
+
 {
 echo "--------------------------------------------------------------------"
 echo "                            USER LOG IN                             "
@@ -123,30 +177,31 @@ userName="customer1"
 password="pass"
 
 while true
-do
+	do
 
 case $option in
+
 1)
 
 read -p "Enter Username >> " u
 read -p "Enter Password >> " p
 
-if [[ $userName == $u ]] && [[ $password == $p ]]
-then
-echo "$Username, You're authenticated"
-welcomeMessage
-break
-else 
-read -p "Wrong credentials. Do you want change login method!!! Enter Y or N >> " changeLogin
-if [[ $changeLogin == "y" ]]
-then
-loginMethod
-else
-echo "Wrong Credetials. Please enter Again"
-fi
-
-fi
+	if [[ $userName == $u ]] && [[ $password == $p ]]
+		then
+		echo "$Username, You're authenticated"
+		welcomeMessage
+		break
+		else 
+		read -p "Wrong credentials. Do you want change login method!!! Enter Y or N >> " changeLogin
+			if [[ $changeLogin == "y" ]]
+				then
+				loginMethod
+				else
+				echo "Wrong Credetials. Please enter Again"
+			fi
+	fi
 ;;
+
 2)
 echo -e "\nPlease create a new account with us by filling up the mandatory fields"
 createUserDetails
@@ -156,6 +211,7 @@ echo -e "\nCongratulations!! $createUsername is successfully added"
 welcomeMessage
 break
 ;;
+
 3)
 echo -e "\nPlease enter your details as a Guest"
 createUserDetails
@@ -166,8 +222,9 @@ break
 esac
 done
 
-sleep 3
+sleep 5
 clear
+
 echo "--------------------------------------------------------------------"
 echo "                          PICKUP/DELIVERY                           "
 echo "--------------------------------------------------------------------"
@@ -180,18 +237,16 @@ echo "                           "
 read -p "Select which option  you want to perform (1-2) >> " pickupORdelivery
 echo "                           "
 echo -e "\nYou have chosen: $pickupORdelivery"
+
 deliveryCharges=0
 
 
 if [[ $pickupORdelivery ==  2  ]]
-then
-deliveryCharges=5
+	then
+	deliveryCharges=5
 fi
 
 echo -e "\n\$$deliveryCharges are delivery charges"
-
-
-
 
 ################################################################################
 ################################################################################
@@ -249,20 +304,21 @@ echo " Enjoy!								  "
 #               (  (    /          '.__.'
 #                `"`'--'
 #
-sleep 1
+sleep 30
 clear
 
-declare -a wing_size=("Small" "Medium" "Large")
-declare -a wing_size_price=("10" "15" "20")
 echo "-------------------------------------"
 echo "             Wing Sizes              "
 echo "-------------------------------------"
+
 counter=1
+
 for w in ${wing_size[@]}
-do 
-echo -e "\n$counter.$w \t\tPrice: \$${wing_size_price[$counter-1]} "
-((counter++))
+	do 
+	echo -e "\n$counter.$w \t\tPrice: \$${wing_size_price[$counter-1]} "
+	((counter++))
 done
+
 echo " "
 echo "-------------------------------------"
 echo " "
@@ -272,30 +328,32 @@ read -p "How many do you need >>" total_wing_quantity
 echo "                                   "
 
 selected_wing_size="${wing_size[$wing_size_selection-1]}"
+
 echo "Your Choice was a size $selected_wing_size order."
 echo " "
 echo  "Quantity : $total_wing_quantity"
 echo " "
 echo  "Price : \$${wing_size_price[$wing_size_selection-1]}"
+
 price_for_selected_wing_size=$(( "$total_wing_quantity * ${wing_size_price[$wing_size_selection-1]}" ))
-echo "                                          "
-sleep 3
+
+sleep 5
 clear
 
 # for selection of protein
 
-declare -a protein_options=("Regular" "Drum_Only" "Flat_Only" "Breaded_Cauliflower")
-declare -a protein_options_price=("0" "0" "0" "0")
-
 echo "------------------------------------------"
 echo "          CHOICE OF PROTEIN               "
 echo "------------------------------------------"
+
 counter=1
+
 for p in ${protein_options[@]}
-do 
-echo -e "\n$counter.$p" # not needed ==>  \t\tPrice: \$${protein_options_price[$counter-1]} "
-((counter++))
+	do 
+	echo -e "\n$counter.$p" # not needed ==>  \t\tPrice: \$${protein_options_price[$counter-1]} "
+	((counter++))
 done
+
 echo " "
 echo "------------------------------------------"
 echo "                                "
@@ -303,6 +361,7 @@ read -p "Please Select an Option [1-4]: > " protein_options_selection
 echo "                                      "
 
 selected_protein="${protein_options[$protein_options_selection-1]}"
+
 echo "Your Choice of Protein is $selected_protein"
 echo "                                        "
 #echo "Price : ${protein_options_price[$protein_options_selection-1]}"
@@ -312,59 +371,60 @@ price_for_selected_protein="${protein_options_price[$protein_options_selection-1
 #echo "Total Price is $price_for_selected_protein"
 echo "                                       "
 
-sleep 3
+sleep 5
 clear
 
 #for selection of sauce
 
-declare -a sauce_options=("Buffalo" "BBQ" "ThaiCurry" "SpicyGarlic")
-declare -a sauce_options_price=("0" "0" "0" "0")
-
 echo "------------------------------------------"
 echo "            CHOICE OF SAUCE               "
 echo "------------------------------------------"
+
 counter=1
 
 for s in ${sauce_options[@]}
-do 
-echo -e "\n$counter.$s" # again not needed no price \t\tPrice: \$${sauce_options_price[$counter-1]} "
-((counter++))
+	do 
+	echo -e "\n$counter.$s" # again not needed no price \t\tPrice: \$${sauce_options_price[$counter-1]} "
+	((counter++))
 done
+
 echo " "
 echo "------------------------------------------"
 read -p "Please Select an Option [1-4]: > " sauce_options_selection
 echo "                                      "
+
 selected_sauce="${sauce_options[$sauce_options_selection-1]}"
+
 echo "Your Choice of Sauce is $selected_sauce"
 echo "                                  "
+
 #echo "Price : ${sauce_options_price[$sauce_options_selection-1]}"
-
 #read -p "How many do you need >>" total_wing_quantity
-
 #echo -e "\nQuantity : $total_wing_quantity"
 #echo -e "\nPrice : $price_for_selected_sauce"
+
 price_for_selected_sauce="${sauce_options_price[$sauce_options_selection-1]}" 
+
 #echo "Total Price is $price_for_selected_sauce"
 #echo "                                                  "
 
-sleep 3
+sleep 5
 clear
 
 # for selection of side
 
-declare -a side_options=("PotatoWedges" "Fries" "Extra_Sauce" "Celery" "Extra_Wings" "No_Side")
-declare -a side_options_price=("3" "2" "1" "1" "3" "0")
-
 echo "------------------------------------------"
 echo "            CHOICE OF SIDE                "
 echo "------------------------------------------"
+
 counter=1
 
 for si in ${side_options[@]}
-do 
-echo -e "\n$counter.$si \t\t\tPrice: \$${side_options_price[$counter-1]} "
-((counter++))
+	do 
+	echo -e "\n$counter.$si \t\t\tPrice: \$${side_options_price[$counter-1]} "
+	((counter++))
 done
+
 echo " "
 echo "------------------------------------------"
 read -p "Please Select an Option [1-4]: > " side_options_selection
@@ -373,6 +433,7 @@ read -p "How many do you need >>" total_side_quantity
 echo "                                   "
 
 selected_side="${side_options[$side_options_selection-1]}"
+
 echo "You picked our $selected_side side today."
 echo " "
 echo  "Quantity : $total_side_quantity"
@@ -381,26 +442,23 @@ echo  "Price : \$${side_options_price[$side_options_selection-1]}"
 price_for_selected_side=$(( "$total_side_quantity * ${side_options_price[$side_options_selection-1]}" ))
 echo "                                     "
 
-sleep 3
+sleep 5
 clear
 
 # for selection of dresssing
 
-
-declare -a dressing_options=("Blu_Cheese" "Ranch" "No_Dressing")
-declare -a dressing_options_price=("0" "0" "0")
-
-
 echo "------------------------------------------"
 echo "          CHOICES OF DRESSING             "
 echo "------------------------------------------"
+
 counter=1
 
 for d in ${dressing_options[@]}
-do 
-echo -e "\n$counter.$d" # no price \t\tPrice: \$${dressing_options_price[$counter-1]} "
-((counter++))
+	do 
+	echo -e "\n$counter.$d" # no price \t\tPrice: \$${dressing_options_price[$counter-1]} "
+	((counter++))
 done
+
 echo " "
 echo "------------------------------------------"
 read -p "Please Select an Option [1-3]: > " dressing_options_selection
@@ -416,24 +474,23 @@ echo " "
 price_for_selected_dressing="${dressing_options_price[$dressing_options_selection-1]}" 
 #echo "                                                 "
 
-sleep 3
+sleep 5
 clear
 
 # for selection of beverages
 
-declare -a beverage_options=("Soda" "SweetTea" "NormalTea" "H2O")
-declare -a beverage_options_price=("2" "3" "2" "1")
-
 echo "------------------------------------------"
 echo "          CHOICE OF BEVERAGE              "
 echo "------------------------------------------"
+
 counter=1
 
 for b in ${beverage_options[@]}
-do 
-echo -e "\n$counter.$b \t\t\tPrice: \$${beverage_options_price[$counter-1]} "
-((counter++))
+	do 
+	echo -e "\n$counter.$b \t\t\tPrice: \$${beverage_options_price[$counter-1]} "
+	((counter++))
 done
+
 echo " "
 echo "-----------------------------------------"
 read -p "Please Select an Option [1-4]: >> " beverage_options_selection
@@ -442,15 +499,16 @@ read -p "How many do you need >>" total_beverage_quantity
 echo "                                   "
 
 selected_beverage="${beverage_options[$beverage_options_selection-1]}"
+
 echo "Your Refresher you picked is $selected_beverage"
 echo " "
 echo  "Quantity : $total_beverage_quantity"
 echo " "
 echo  "Price : \$${beverage_options_price[$beverage_options_selection-1]}"
-price_for_selected_beverage=$(( "$total_beverage_quantity * ${beverage_options_price[$beverage_options_selection-1]}" ))
-echo "                                               "
 
-sleep 3
+price_for_selected_beverage=$(( "$total_beverage_quantity * ${beverage_options_price[$beverage_options_selection-1]}" ))
+
+sleep 5
 clear
 
 ################################################################################
@@ -476,11 +534,13 @@ echo  "Choice of Beverage             - $selected_beverage with $total_beverage_
 echo "                                   " 
 read -p "Is this your Order? Yes/No >> " confirm_order
 if [[ "$confirm_order" == "y" ]]
-then
-echo "My Pleasure, Please proceed to checkout"
+	then
+	echo "My Pleasure, Please proceed to checkout"
 fi
+
 echo "Please Wait, Adding Items to Your Cart...                                            "
-sleep 3
+
+sleep 5
 clear
 
 ################################################################################
@@ -506,14 +566,13 @@ echo "You have chosen $cashORcard"
 
 
 if [[ $cashORcard ==  2  ]]
-then
-read -p "Enter your card number >> " cardNumber
-read -p "Enter Expiration date of the card >> " expireDate
-read -p "Enter cvv number >> " cvvNum
-echo -e "\nYour Payment information is stored and being verified"
-echo -e "\nGreat! Your card has been verified.Please proceed with Total Payment"
+	then
+	read -p "Enter your card number >> " cardNumber
+	read -p "Enter Expiration date of the card >> " expireDate
+	read -p "Enter cvv number >> " cvvNum
+	echo -e "\nYour Payment information is stored and being verified"
+	echo -e "\nGreat! Your card has been verified.Please proceed with Total Payment"
 fi
-echo "                                                 "
 
 order_cost=$(( $price_for_selected_wing_size + $price_for_selected_protein + $price_for_selected_side + $price_of_selected_sauce + $price_for_selected_dressing + $price_for_selected_beverage ))
 
@@ -561,13 +620,13 @@ sleep 3
 echo "Gathering Time Request from Kitchen..."
 sleep 3
 echo "..."
-sleep 3
+sleep 2
 echo "......"
-sleep 3
+sleep 2
 echo "........."
-sleep 3
+sleep 1
 echo "............"
-sleep 3
+sleep 1
 echo -e "\nYour order has been confirmed and it will be ready in 20 minutes!"
 sleep 5
 clear
@@ -580,7 +639,7 @@ echo " "
 echo -e "\nPlease feel free to contact us at any time @ 571-245-4647."
 echo "                                               "
 
-sleep 5
+sleep 15
 clear
 
 ################################################################################
@@ -596,15 +655,15 @@ echo "-------------------------------------------------------------------------"
 echo "   "
 echo "Thank you for your purchase."
 echo "                             "
-sleep 2
+sleep 10
 echo "Here at WingIn It, we value each and every customer."
 echo "                                 "
-sleep 2
+sleep 10
 echo "For more than 25 years,our company has been focused on providing the best-ever fried chicken experience, along with attentive service to valued customers."
 echo "                                     "
-sleep 2
+sleep 10
 echo "We appreciate having you as a customer and look forward to serving you again."
 echo "                                                                      "
-sleep 2
+sleep 10
 clear
 figlet THANK YOU , KEEP WING'IN WITH US
